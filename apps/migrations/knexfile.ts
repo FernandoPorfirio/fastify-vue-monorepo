342484b70fad12f6a0ad54e5cefd622f0a2df994
Fernando Porfirio <fernando.porfirio@yetz.com.br>
2026-01-18 20:54:44 -0300

    feat: add knexfile.ts for Knex configuration with database connection and migration settings

---
+++ apps/migrations/knexfile.ts
@@ -0,0 +1,22 @@
+import type { Knex } from 'knex';
+
+const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME
+    },
+    migrations: {
+      directory: './migrations',
+      tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    }
+  }
+};
+
+export default config;
