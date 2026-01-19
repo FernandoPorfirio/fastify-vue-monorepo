be6fb517f4d1fc10cd2767707a00f6c1a8babaa2
Fernando Porfirio <fernando.porfirio@yetz.com.br>
2026-01-18 21:20:19 -0300

    feat: update server configuration with error handler and refined CORS methods

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -7,16 +7,18 @@ import {
 } from 'fastify-type-provider-zod'
 import { fastifySwagger } from '@fastify/swagger'
 import { fastifyCors } from '@fastify/cors'
-import ScalarApiReference from '@scalar/fastify-api-reference'
+import { errorHandler } from './errors/handler'
+import { registerRoutes } from './routes'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
+app.setErrorHandler(errorHandler)
 
 app.register(fastifyCors, {
   origin: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   // credentials: true
 })
 
@@ -31,9 +33,7 @@ app.register(fastifySwagger, {
   transform: jsonSchemaTransform,
 })
 
-app.register(ScalarApiReference, {
-  routePrefix: '/api-docs'
-})
+app.register(registerRoutes)
 
 app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('Server running on http://localhost:3333')
